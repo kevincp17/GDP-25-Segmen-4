073d0247067e450121981f99e4bbcb9ea04d1a31
ApriliaPurwanto <aprilia.119140003@student.itera.ac.id>
2023-11-09 20:34:07 +0700

    first

--- project/src/main/java/com/portal/project/model/Interview.java
+++ project/src/main/java/com/portal/project/model/Interview.java
@@ -1,10 +1,10 @@
-package com.portal.project.model;
+// package com.portal.project.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+// import javax.persistence.Entity;
+// import javax.persistence.Table;
 
-@Entity
-@Table
-public class Interview {
+// @Entity
+// @Table
+// public class Interview {
     
-}
+// }
