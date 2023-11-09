e6444839995bdd57840bb3537765dc714c189e86
alshanabilla <alshanabilla17@gmail.com>
2023-11-09 13:54:25 +0700

    updated repo

--- project/src/main/java/com/portal/project/repository/UserRepository.java
+++ project/src/main/java/com/portal/project/repository/UserRepository.java
@@ -1,11 +1,9 @@
 package com.portal.project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.portal.project.model.User;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
-    @Query(value = "select u.email, r.name FROM tb_m_user u JOIN tb_m_role r ON u.role_id = r.role_id where r.name = 'ta'", nativeQuery = true)
-    public User findTaByUser();
+    
 }
