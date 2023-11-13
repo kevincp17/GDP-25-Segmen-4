2278a63addbc259c6810e99c2dfe68f5773aa195
ApriliaPurwanto <aprilia.119140003@student.itera.ac.id>
2023-11-13 11:23:22 +0700

    ts

--- project/src/main/java/com/portal/project/model/InterviewUser.java
+++ project/src/main/java/com/portal/project/model/InterviewUser.java
@@ -1,114 +1,120 @@
-// package com.portal.project.model;
+package com.portal.project.model;
 
-// import java.util.Date;
+import java.time.LocalDateTime;
+import java.util.Date;
 
-// import javax.persistence.Column;
-// import javax.persistence.Entity;
-// import javax.persistence.GeneratedValue;
-// import javax.persistence.GenerationType;
-// import javax.persistence.Id;
-// import javax.persistence.JoinColumn;
-// import javax.persistence.ManyToOne;
-// import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
-// @Entity
-// @Table(name = "tb_tr_interview_user")
-// public class InterviewUser {
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     @Column(name = "interview_user_id")
-//     private Integer interview_user_id;
-//     private Date interview_date;
-//     private String link;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name = "tb_tr_interview_user")
+public class InterviewUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "interview_user_id")
+    private Integer interview_user_id;
+    @DateTimeFormat(pattern ="yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime interview_date;
+    private String link;
+    private String interview_type;
      
-//     @ManyToOne
-//     @JoinColumn(name = "interview_id")
-//     private Interview interview;
+    @ManyToOne
+    @JoinColumn(name = "interview_id")
+    private Interview interview;
 
-//     @ManyToOne
-//     @JoinColumn(name = "applicant_id", referencedColumnName = "user_id")
-//     private User applicant;
+    @ManyToOne
+    @JoinColumn(name = "applicant_id", referencedColumnName = "user_id")
+    private User applicant;
 
-//     @ManyToOne
-//     @JoinColumn(name = "ta_id", referencedColumnName = "user_id")
-//     private User ta;
+    @ManyToOne
+    @JoinColumn(name = "ta_id", referencedColumnName = "user_id")
+    private User ta;
 
-//     @ManyToOne
-//     @JoinColumn(name = "trainer_id", referencedColumnName = "user_id")
-//     private User trainer;
+    @ManyToOne
+    @JoinColumn(name = "trainer_id", referencedColumnName = "user_id")
+    private User trainer;
 
-//     @ManyToOne
-//     @JoinColumn(name = "status_id")
-//     private Status status;
+    @ManyToOne
+    @JoinColumn(name = "status_id")
+    private Status status;
 
-//     public Integer getInterview_user_id() {
-//         return interview_user_id;
-//     }
+    public Integer getInterview_user_id() {
+        return interview_user_id;
+    }
 
-//     public void setInterview_user_id(Integer interview_user_id) {
-//         this.interview_user_id = interview_user_id;
-//     }
+    public void setInterview_user_id(Integer interview_user_id) {
+        this.interview_user_id = interview_user_id;
+    }
 
-//     public Date getInterview_date() {
-//         return interview_date;
-//     }
+    public String getLink() {
+        return link;
+    }
 
-//     public void setInterview_date(Date interview_date) {
-//         this.interview_date = interview_date;
-//     }
+    public void setLink(String link) {
+        this.link = link;
+    }
 
-//     public String getLink() {
-//         return link;
-//     }
+    public Interview getInterview() {
+        return interview;
+    }
 
-//     public void setLink(String link) {
-//         this.link = link;
-//     }
+    public void setInterview(Interview interview) {
+        this.interview = interview;
+    }
 
-//     public Interview getInterview() {
-//         return interview;
-//     }
+    public User getApplicant() {
+        return applicant;
+    }
 
-//     public void setInterview(Interview interview) {
-//         this.interview = interview;
-//     }
+    public void setApplicant(User applicant) {
+        this.applicant = applicant;
+    }
 
-//     public User getApplicant() {
-//         return applicant;
-//     }
+    public User getTa() {
+        return ta;
+    }
 
-//     public void setApplicant(User applicant) {
-//         this.applicant = applicant;
-//     }
+    public void setTa(User ta) {
+        this.ta = ta;
+    }
 
-//     public User getTa() {
-//         return ta;
-//     }
+    public User getTrainer() {
+        return trainer;
+    }
 
-//     public void setTa(User ta) {
-//         this.ta = ta;
-//     }
+    public void setTrainer(User trainer) {
+        this.trainer = trainer;
+    }
 
-//     public User getTrainer() {
-//         return trainer;
-//     }
+    public Status getStatus() {
+        return status;
+    }
 
-//     public void setTrainer(User trainer) {
-//         this.trainer = trainer;
-//     }
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 
-//     public Status getStatus() {
-//         return status;
-//     }
+    public LocalDateTime getInterview_date() {
+        return interview_date;
+    }
 
-//     public void setStatus(Status status) {
-//         this.status = status;
-//     }
+    public void setInterview_date(LocalDateTime interview_date) {
+        this.interview_date = interview_date;
+    }
 
-    
+    public String getInterview_type() {
+        return interview_type;
+    }
 
-    
-    
-    
-    
-// }
+    public void setInterview_type(String interview_type) {
+        this.interview_type = interview_type;
+    } 
+}
